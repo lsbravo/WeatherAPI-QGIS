4fae0fc30759f18cc4a5cac716b8d5817301b464
Lee Bravo <121823541+lsbravo@users.noreply.github.com>
2024-04-30 18:59:24 -0500

    Update main.py
    This update has the GeoJSON file save automatically to the current working directory of the Python file. The name is set to the date of download for easier automation on daily runs.

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import requests
+from datetime import datetime
+import os
 import json
 from geojson import Feature, Point, FeatureCollection, dump
 
+#Function to Build the Filepath to Save the GEOJSON Files
+
+def FileName():
+    filepath = os.getcwd()
+    currentDateAndTime = datetime.now()
+    name = "AreaTemp-"+str(currentDateAndTime.year)+str(currentDateAndTime.month) + str(currentDateAndTime.day)+".json"
+    geojsonFile = os.path.join(filepath,name)
+    return geojsonFile
+
+#Function to get the grid number from the latitude and longitude
 def pullURLfromCoords(x,y):
     APIURL = "https://api.weather.gov/points/" + str(x)+"," +str(y)
     response =  requests.get(APIURL) 
     GridURL = response.json()["properties"]["forecast"]
     return GridURL
 
-
+#Function to pull the forecast from the grid and return as a feature a town
 def pullForecast(PlaceName, URL):
     response =  requests.get(URL) 
     WeeklyData = response.json()
     WeeklyData["properties"]["updated"]
     Details = WeeklyData["properties"]["periods"]
-
     #This dictionary holds temperatures for every day of the week
     ExportInfo = {}
-
     for x in Details:
         date = x['name']    
         ExportInfo[date] = x['temperature']
-
     Town = Point((29.086575, -95.279697))
     TownFeature = Feature(geometry=Town, properties={"Name":PlaceName,"Temperature": ExportInfo["Tonight"]})
     return TownFeature
@@ -37,18 +46,18 @@ towns=[]
 
 for place in CoordDict:
     ForecastURL = pullURLfromCoords(CoordDict[place][0], CoordDict[place][1])
-    print(ForecastURL)
     featuredata = pullForecast(place, ForecastURL)
-    print(featuredata)
     FeatureDict[place] = featuredata
     towns.append(featuredata)
-    
-print(FeatureDict)
 
+geoFile = FileName()
+regionaldata = FeatureCollection(towns)
+
+with open(geoFile, 'w') as f:
+    dump(regionaldata,f)
+
+
+#THIS LINE IS SPECIFICALLY FOR USE WITHIN QGIS
 #Import the GEOSON to QGIS
 vlayer = QgsVectorLayer(r"C:\Users\Chabl\Desktop\AreaTemperatures.geojson","mygeojson","ogr")
 QgsProject.instance().addMapLayer(vlayer)
-
-regionaldata = FeatureCollection(towns)
-with open(r'C:\Users\Desktop\AreaTemperatures.geojson', 'w') as f:
-    dump(regionaldata,f)
